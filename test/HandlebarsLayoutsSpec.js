1f521d8f9cb706563b0c5575b77b37704c4b3cb1
ipaterson <ipaterson@newstex.com>
2014-05-30 10:46:21 -0400

    Initialize _blocks in the context if it does not exist. This allows view and layout templates to be composed programmatically when required. For example, express3-handlebars renders the view to an HTML string and passes this to the layout by mixing {body: renderedBody} into the context. After rendering the view, that context now also has _blocks from handlebars-layouts. This allows any blocks defined in the layout to be modified in the view.

--- test/HandlebarsLayoutsSpec.js
+++ test/HandlebarsLayoutsSpec.js
@@ -10,6 +10,7 @@ describe('handlebars-layouts', function () {
 
     // Register Partials
     handlebars.registerPartial('layout', fs.readFileSync(__dirname + '/fixtures/layout.hbs', 'utf8'));
+    handlebars.registerPartial('layout_simple', fs.readFileSync(__dirname + '/fixtures/layout_simple.hbs', 'utf8'));
     handlebars.registerPartial('sublayout', fs.readFileSync(__dirname + '/fixtures/sublayout.hbs', 'utf8'));
     handlebars.registerPartial('partial1', fs.readFileSync(__dirname + '/fixtures/partial1.hbs', 'utf8'));
     handlebars.registerPartial('partial2', fs.readFileSync(__dirname + '/fixtures/partial2.hbs', 'utf8'));
@@ -36,5 +37,30 @@ describe('handlebars-layouts', function () {
         var template = handlebars.compile(fs.readFileSync(__dirname + '/fixtures/template.hbs', 'utf8'));
 
         expect(template(data)).toBe(fs.readFileSync(__dirname + '/expected/output.html', 'utf8'));
+    });
+
+    it('should properly render programmatically-composed layouts', function () {
+        var data = {};
+        var template = handlebars.compile(fs.readFileSync(__dirname + '/fixtures/template_non_extending.hbs', 'utf8'));
+
+        // Render the template, which populates data._blocks
+        data.body = template(data);
+        expect(data._blocks).toBeDefined();
+
+        var layout = handlebars.compile(fs.readFileSync(__dirname + '/fixtures/layout_simple.hbs', 'utf8'));
+        expect(layout(data)).toBe(fs.readFileSync(__dirname + '/expected/output_simple.html', 'utf8'));
+    });
+
+    it('should properly render self-modifying layouts', function () {
+        var data = {};
+        var template = handlebars.compile(fs.readFileSync(__dirname + '/fixtures/layout_self_modifying.hbs', 'utf8'));
+        expect(template(data)).toBe(fs.readFileSync(__dirname + '/expected/output_self_modifying.html', 'utf8'));
+    });
+
+    it('should properly render layouts included as partials', function () {
+        var data = {};
+        var template =
+            handlebars.compile(fs.readFileSync(__dirname + '/fixtures/template_including_layout.hbs', 'utf8'));
+        expect(template(data)).toBe(fs.readFileSync(__dirname + '/expected/output_simple.html', 'utf8'));
     });
 });
